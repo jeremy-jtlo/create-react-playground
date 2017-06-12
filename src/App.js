1cd1b947b0f8a11f4d63fd45dae3655b040dbe1a
Jeremy Lo <y2kfirzen@gmail.com>
2017-06-11 21:28:48 -0700

    Stripping comments from App.js

--- src/App.js
+++ src/App.js
@@ -4,11 +4,9 @@ import DataArea from './components/data-area';
 class App extends Component {
   render() {
     return (
-      // Add a component row for name/credentials
       <div>
         <DataArea />
       </div>
-      // Currently content needs to be hacked to 80% width to simulate margins. Fix?
     );
   }
 }
