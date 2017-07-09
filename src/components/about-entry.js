d47328e05dc8d160d9c99a56277f88f8a2782df9
Jeremy Lo <y2kfirzen@gmail.com>
2017-07-08 17:13:28 -0700

    Refactoring buttons into their own component

--- src/components/about-entry.js
+++ src/components/about-entry.js
@@ -1,8 +1,22 @@
 import React, {Component} from 'react';
+import Button from './button.js';
 
 export default class AboutEntry extends Component {
     render () {
         var about_content = require("../data/about.json");
+        var button_array = [];
+        
+        for (var i=0; i<Object.keys(about_content["buttons"]).length; i++) {
+            var current_item = about_content["buttons"][i]
+            button_array.push(
+                <Button 
+                    extra_class="col-4of12"
+                    link={current_item["link"]}
+                    icon={current_item["icon"]}
+                    text={current_item["string"]}
+                />
+            );
+        }
         return (
             <div className="flex-container">
                 <div className="col-12of12">
@@ -10,9 +24,7 @@ export default class AboutEntry extends Component {
                     
                 </div>
                 <div className="col-4of12 flex-container">
-                    <a className="button col-4of12" href="#"><i className="fi-social-linkedin size-16"> </i>Linkedin </a>
-                    <a className="button col-4of12" href="#"><i className="fi-social-github size-16"> </i>GitHub </a>
-                    <a className="button col-4of12" href="#"><i className="fi-mail size-16"> </i>Email Me </a>
+                    {button_array}
                 </div>
                 <div className="col-8of12"> 
                     <p>{about_content.content}</p>
---
+++ src/components/button.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+export default class Button extends Component {
+    render() {
+        var class_name = "button " + this.props.extra_class;
+        return(
+            <a className={class_name} href={this.props.link}>
+                <i className={this.props.icon}> </i>
+                {this.props.text}
+            </a>
+        );
+    }
+}
